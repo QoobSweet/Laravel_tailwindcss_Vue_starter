cbe15ffc2a23d642c5324a8e5cdb3d83f6381a64
Kevin <QoobSweet@gmail.com>
2023-05-11 19:24:46 -0400

    initial commit. User registration and login only

---
+++ resources/js/Components/PlatformList.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+defineProps({
+    tableData: Array,
+});
+</script>
+<template>
+    <div class="p-2">
+        <table class="w-full">
+            <thead>
+            <tr class="border-b-2">
+                <th class="text-left pl-2">Name</th>
+                <th class="text-center">Number of Projects</th>
+                <th class="text-center">Number of Users</th>
+                <th class="text-center">Manage Platform</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="item in tableData" :key="item.name" class="bg-white">
+                <td class="text-left pl-2">{{ item.name }}</td>
+                <td class="text-center">{{ item.numProjects }}</td>
+                <td class="text-center">{{ item.numUsers }}</td>
+                <td class="text-center">{{ item.managePlatform }}</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
---
+++ resources/js/Components/Welcome.vue
@@ -0,0 +1,29 @@
+<script setup>
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import PlatformList from "./PlatformList.vue";
+</script>
+
+<template>
+    <div>
+        <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+            <ApplicationLogo class="block h-12 w-auto" />
+        </div>
+
+        <div class="bg-gray-100">
+            <PlatformList :table-data="[
+                {
+                    name: 'Example 1',
+                    numProjects: 5,
+                    numUsers: 10,
+                    managePlatform: 'Link 1',
+                },
+                {
+                    name: 'Example 2',
+                    numProjects: 3,
+                    numUsers: 8,
+                    managePlatform: 'Link 2',
+                }
+            ]"/>
+        </div>
+    </div>
+</template>
